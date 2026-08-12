b97fccb91a90a33db8badfe981b9659de0182b14
keerthan <poojarykeerthan571@gmail.com>
2026-08-12 22:11:01 +0530

    Fix MongoDB environment variable

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -3,7 +3,7 @@ import os
 
 load_dotenv()
 
-MONGODB_URL = os.getenv("MONGODB_URI")
+MONGODB_URL = os.getenv("MONGODB_URL")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
