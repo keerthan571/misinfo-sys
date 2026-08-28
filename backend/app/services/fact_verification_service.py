9cbeab087fbebc79218231860683f3f28c0b45c4
keerthan <poojarykeerthan571@gmail.com>
2026-08-29 00:04:52 +0530

    updated groq version

--- backend/app/services/fact_verification_service.py
+++ backend/app/services/fact_verification_service.py
@@ -14,10 +14,12 @@ logger = logging.getLogger(__name__)
 
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
 GROQ_MODEL = os.getenv(
     "GROQ_MODEL",
-    "llama-3.3-70b-versatile"
+    "openai/gpt-oss-20b"
 )
+
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
 
